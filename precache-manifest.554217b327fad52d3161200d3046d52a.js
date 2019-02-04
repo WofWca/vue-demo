b6422971121a4fd477e1e792214159f0095a2c13
WofWca <wofwca@gmail.com>
2019-02-04 16:15:11 +0800

    Updates

---
+++ precache-manifest.554217b327fad52d3161200d3046d52a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85229001eaca63d6983d",
+    "url": "/vue-demo/css/app.d0d4628a.css"
+  },
+  {
+    "revision": "85229001eaca63d6983d",
+    "url": "/vue-demo/js/app.0d2c2112.js"
+  },
+  {
+    "revision": "c6f3ab0efc6caa150464",
+    "url": "/vue-demo/css/chunk-vendors.c590eba0.css"
+  },
+  {
+    "revision": "c6f3ab0efc6caa150464",
+    "url": "/vue-demo/js/chunk-vendors.a6e4100f.js"
+  },
+  {
+    "revision": "5fd0620bb4c845280f4cd7ab13199960",
+    "url": "/vue-demo/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-demo/robots.txt"
+  }
+];
